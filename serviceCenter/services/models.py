f4df72e2dc66f65e0411354da67974ecb7a53814
olezh <olezhka1629@gmail.com>
2023-06-01 01:55:03 +0300

    start coding models

--- serviceCenter/services/models.py
+++ serviceCenter/services/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Device(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    image = models.ImageField(upload_to='не решил ещё', blank=True)
+
+    TYPES = ["laptop",
+             "phone",
+             "PC",
+             "tablet"]
+
+    device_type = models.CharField(max_length=50, choices=TYPES)
+
+class Client(models.Model):
+
+    first_name = models.CharField(max_length=200)
+
+    last_name = models.CharField(max_length=200)
+
+    date_of_birth = models.DateField()
+
+    email = models.EmailField()
+
+    phone_number = models.CharField(max_length=50,)
