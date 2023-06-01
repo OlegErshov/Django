a64a52c5b2bfb2364195b58d1b90650b5850a23f
olezh <olezhka1629@gmail.com>
2023-06-02 01:11:11 +0300

    add styls and html to main page

--- serviceCenter/services/models.py
+++ serviceCenter/services/models.py
@@ -1,18 +1,33 @@
 from django.db import models
 
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
 class Device(models.Model):
 
     name = models.CharField(max_length=100)
 
-    image = models.ImageField(upload_to='не решил ещё', blank=True)
+    image = models.ImageField(upload_to='device/%Y/%m/%d', blank=True)
 
-    TYPES = ["laptop",
-             "phone",
-             "PC",
-             "tablet"]
+    device_category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
-    device_type = models.CharField(max_length=50, choices=TYPES)
+    def __str__(self):
+        return self.name
+
+
+class Issue(models.Model):
+    issue_type = models.CharField(max_length=100)
+
+    device_type = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    def __str__(self):
+        return self.issue_type
 
 class Client(models.Model):
 
@@ -25,3 +40,6 @@ class Client(models.Model):
     email = models.EmailField()
 
     phone_number = models.CharField(max_length=50,)
+
+    def __str__(self):
+        return self.email
--- serviceCenter/services/views.py
+++ serviceCenter/services/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'services/mainPage.html')
 
+
+def services(request):
+    return render(request, 'services/ourServices.html')
